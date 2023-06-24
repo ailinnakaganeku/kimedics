82a2d3dbd58bf785d1f6973d95bc83df2b5a4d75
ailinnakaganeku <ailin.nakaganeku@gmail.com>
2023-06-24 06:49:21 -0300

    testing the API

--- src/components/Drawer/index.jsx
+++ src/components/Drawer/index.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
 import SearchIcon from '@mui/icons-material/Search';
-import { Column, OptionContainer, OptionIcon, StyledDrawer, StyledSearchField, StyledTextField } from './styes';
+import { Column, OptionContainer, OptionIcon, StyledDrawer, StyledSearchField, StyledTextField } from './styles';
 import PracticeList from '../Practice';
-import { data } from '../Practice/data';
 
 const options = [
     {
@@ -16,6 +15,28 @@ const options = [
 
 const Drawer = () => {
     const [searchQuery, setSearchQuery] = useState('');
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://s2ju1fj7u1.execute-api.us-east-2.amazonaws.com/Prod/api/practices', {
+                mode: 'no-cors',
+            });
+
+            if (!response.ok) {
+                throw new Error('Error fetching data');
+            }
+
+            const responseData = await response.json();
+            setData(responseData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     const renderOptions = () => {
         const filteredOptions = options.filter((option) =>
